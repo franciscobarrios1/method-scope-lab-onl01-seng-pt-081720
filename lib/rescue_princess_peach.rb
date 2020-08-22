c18a547ec1f2b69c90f3de9ba99367103bc8a22f
Francisco barrios <frankybarrios1@icloud.com>
2020-08-22 19:10:41 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,5 +1,5 @@
-
-## Comment the below back in to see method scope in action!
-
-# rescue_princess_peach
-# puts status
+```ruby
+def rescue_princess_peach
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
+end
